3d532e913e09f5272b579ba7efec5541b3914ab7
Gabriel Silveira <ramos97033@outlook.com>
2024-03-12 13:29:46 -0300

    incluindo Loading

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,8 @@
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import "./Loading.css"
+
+
+
+export default function Loading(){
+    return <AiOutlineLoading3Quarters className="loading"/>
+}
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,27 +2,29 @@ import { useEffect, useState } from "react"
 import "./Products.css"
 import fetchProducts from "../../api/fetchproducts"
 import ProductCard from "../ProductCard/ProductCard"
+import Loading from "../Loading/Loading"
 
 
 
 export default function Products(){
 
     const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
 
 
     useEffect(() => {
-        fetchProducts('iphone').then((response) => [
-            setProducts(response)
-        ])
+        fetchProducts('iphone').then((response) => {
+            setProducts(response);
+            setLoading(false)
+            
+        })
     }, [])
     
 
     return(
-        
-        <section className="products container">
+        (loading ? <Loading/> : <section className="products">
             {products.map((produto) => <ProductCard key={products.id} data={produto}/>)}
-        </section>
+        </section> 
+        )
         
-        
-    )
-}
+)}
